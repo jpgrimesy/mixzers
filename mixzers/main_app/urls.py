247bd7f7a63d3fff03f60e699589da59c2aa8d2d
Melrose Ceballos <mcm44@hawaii.edu>
2023-03-31 14:07:28 -0700

    created job post and near job paths

--- mixzers/main_app/urls.py
+++ mixzers/main_app/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('job_post/', views.near_job, name="job_post"),
+    path('near_job/', views.near_job, name='near_job'),
 
 ]
--- mixzers/main_app/views.py
+++ mixzers/main_app/views.py
@@ -5,6 +5,15 @@ from django.shortcuts import render
 # HOME PAGE
 def home(request):
     return render(request, 'home.html')
-
+    
+# ABOUT PAGE    
 def about(request):
     return render(request, 'about.html')
+
+# POST A JOB PAGE
+def job_post(request):
+    return render(request, 'jobpost.html')
+
+# NEAR JOB PAGE
+def near_job(request):
+    return render(request, 'nearjob.html')
