dc0f17affc9bece4b21d97c3714bce612ea8d59f
jpgrimesy <japangilinan@gmail.com>
2023-03-31 16:10:41 -0700

    mixzer user profile added to models. gitignore added

--- mixzers/main_app/models.py
+++ mixzers/main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Mixzer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    location = models.CharField(max_length=100)
+    is_student = models.BooleanField(default=False)
+    college = models.CharField(max_length=100, blank=True)
+
+    def verify_student(self):
+        return self.user.email.endswith('.edu')
