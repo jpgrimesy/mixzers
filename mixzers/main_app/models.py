cf271c2812d38f14124cf5fc7fb4791808897aba
ebmiller510 <llhappimess@gmail.com>
2023-03-31 17:48:59 -0500

    updated urls

--- mixzers/main_app/models.py
+++ mixzers/main_app/models.py
@@ -8,37 +8,32 @@ class Employer(models.Model):
     name = models.CharField(max_length=250)
     # location
 
+
 # STUDENT MODEL
 # emily: to be completed on her end
-
-
 class Student (models.Model):
     name = models.CharField(max_length=250)
     # location
     # primary key to be incorporated
 
+
 # MESSAGE MODEL
-
-
 class Message(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField(max_length=250)
     # sender_id with foriegn key to be added
     # recipient_id with foriegn key to be added
 
+
 # REVIEW MODEL
-# emily for review model to commit on her end
-
-
 class Review(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField(max_length=250)
     # reviewer_id with foriegn key to be added
     # reviewee_id with foriegn key to be added
 
+
 # JOB POST MODEL
-
-
 class Job_Post(models.Model):
     title = models.CharField(max_length=250)
     job_description = models.TextField(max_length=250)
