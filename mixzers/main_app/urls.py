b11fa7a95eb009d0eee2e68e9131de3b3511c0c2
jpgrimesy <japangilinan@gmail.com>
2023-03-31 19:50:19 -0700

    user path and function added to views

--- mixzers/main_app/urls.py
+++ mixzers/main_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
+from .models import Mixzer
 
 urlpatterns = [
-
+    path('mixzer/signup/', views.signup, name='signup'),
 ]
--- mixzers/main_app/views.py
+++ mixzers/main_app/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from .forms import UserForm
+from .models import Mixzer
 # Create your views here.
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    # This is how to create a 'user' form object
+    # that includes the data from the browser
+    form = UserCreationForm(request.POST)
+    addtl_form = UserForm(request.POST)
+    if form.is_valid() and addtl_form.is_valid():
+      # This will add the user to the database
+      user = form.save()
+      addtl_user = addtl_form.save(commit=False)
+      addtl_user.user = user
+      addtl_user.save()
+      # This is how we log a user in via code
+      login(request, user)
+      return redirect('/admin/')
+    else:
+      error_message = 'Invalid sign up - try again'
+  # A bad POST or a GET request, so render signup.html with an empty form
+  form = UserCreationForm()
+  addtl_form = UserForm()
+  context = {'form': form, 'add_form': addtl_form ,'error_message': error_message}
+  return render(request, 'test.html', context)
