55c04c3651ac97977de71559c64112ff7c772ba8
jpgrimesy <japangilinan@gmail.com>
2023-03-31 19:48:36 -0700

    mixzer model added

--- mixzers/main_app/models.py
+++ mixzers/main_app/models.py
@@ -4,9 +4,10 @@ from django.contrib.auth.models import User
 
 class Mixzer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    location = models.CharField(max_length=100)
+    location = models.CharField(max_length=100, blank=True)
     is_student = models.BooleanField(default=False)
     college = models.CharField(max_length=100, blank=True)
+    phone_number = models.CharField(max_length=18, blank=True)
 
     def verify_student(self):
         return self.user.email.endswith('.edu')
