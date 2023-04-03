718491c1b4c162fc32ec73c8885f322f4573a295
jpgrimesy <japangilinan@gmail.com>
2023-04-03 13:43:06 -0700

    Add applicants and add candidates path and views added to update job applicants and candidates

--- mixzers/main_app/urls.py
+++ mixzers/main_app/urls.py
@@ -4,8 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('job_post/', views.near_job, name="jobpost"),
-    path('near_job/', views.near_job, name='nearjob'),
+
     # path to the self-reflection profile
     path('profile/', views.profile, name='profile'),
     
@@ -16,4 +15,8 @@ urlpatterns = [
     path('verify/', views.verify, name="verify"),
     path('send-message/<int:user_id>/', views.send_message, name="send_message"),
     path('create-review/<int:user_id>/', views.create_review, name="create_review"),
+    path('post-job/', views.post_job, name='post_job'),
+    path('nearby-jobs/', views.nearby_jobs, name='nearby_jobs'),
+    path('apply/<int:job_id>/', views.apply, name='apply'),
+    path('hire/<int:job_id>/', views.hire, name='hire'),
 ]
