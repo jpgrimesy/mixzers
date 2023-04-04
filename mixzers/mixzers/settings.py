f72c27d75dc27c17368a17ef69a6b332081d705d
ebmiller510 <llhappimess@gmail.com>
2023-04-04 16:13:27 -0500

    added to base li, conditionally rendering nav bar based on login

--- mixzers/mixzers/settings.py
+++ mixzers/mixzers/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+SECRET_KEY = 'django-insecure-u9hnngvn5v5oeqc$kk@2_!47d)w9o@g9&7!^!l@lv)d_x07*ei'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'mixzers.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mixzers',
+        'NAME': 'mix_test',
     }
 }
 
